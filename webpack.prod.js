62d301f7f7e1fa484193ff16f99487e32a51af6e
Emmanuel Vargas <126733106+EmmanuelV22@users.noreply.github.com>
2023-07-02 20:45:54 -0300

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,17 @@
-// const merge = require('webpack-merge');
-// const common = require('./webpack.common.js');
-// const Dotenv = require('dotenv-webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
 
-// module.exports = merge(common, {
-//     mode: 'production',
-//     output: {
-//         publicPath: './'
-//     },
-//     plugins: [
-//         new Dotenv({
-//             safe: true,
-//             systemvars: true
-//         })
-//     ]
-// });
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        publicPath: './src/js/index.js'
+    },
+    plugins: [
+        new Dotenv({
+            safe: true,
+            systemvars: true
+        })
+    ]
+});
 
-module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
-};
